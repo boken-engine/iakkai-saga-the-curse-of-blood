957aa61eaf8ef7070e68fe3c1fbd89ff25548b05
Yeray Rodriguez <yeray.rodriguez@edataconsulting.es>
2021-07-08 07:45:56 +0100

    Base project source

---
+++ src/Iakkai Saga: The curse of Blood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Iakkai Saga: The curse of Blood
+//
+//  Created by Yeray on 8/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
