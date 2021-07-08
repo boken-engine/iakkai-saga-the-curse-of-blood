f0de27dddf19f7c9eb730211720ad86c8a84b8c3
Yeray Rodriguez <yeray.rodriguez@edataconsulting.es>
2021-07-08 08:10:55 +0100

    Fix UI system

---
+++ src/Iakkai Saga - The curse of Blood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iakkai Saga - The Curse of Blood
+//
+//  Created by Yeray on 8/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- test.sh
+++
@@ -1,8 +0,0 @@
-echo "test"
-
-
-files=($"ls -1 raw/**/*")
-for file in $files
-do
-	echo ">>>"$file
-done
