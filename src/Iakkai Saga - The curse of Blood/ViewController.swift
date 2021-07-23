1f4064c52abf14e705ba77e03f5c3810fbe3bb7b
Yeray Rodriguez <yeray.rodriguez@edataconsulting.es>
2021-07-23 14:32:41 +0100

    Better rotation handler

--- src/Iakkai Saga - The curse of Blood/ViewController.swift
+++ src/Iakkai Saga - The curse of Blood/ViewController.swift
@@ -7,18 +7,47 @@
 
 import UIKit
 import BokenEngine
+import os
+
 
 class ViewController: UIViewController {
 
+    var sceneManager: SceneManager? = nil
+    var lastUpdateTime: Int64 = 0;
+    
     override func viewDidLoad() {
+        
+        do {
+            sceneManager = try SceneManager()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
         super.viewDidLoad()
 
-        // Boken Engine initialization
-        let sceneManager = try! SceneManager()
-        sceneManager.setRootView(self.view)
-        try! sceneManager.loadTitleScene()
+        sceneManager!.setRootView(self.view)
+        try! sceneManager!.loadTitleScene()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.deviceHasRotated),
+           name: UIDevice.orientationDidChangeNotification,
+             object: nil)
     }
 
-
+    func printDate() -> String {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss.SSSS"
+        return formatter.string(from: date)
+    }
+    
+    @objc func deviceHasRotated() {
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            let currentTime = Int64(Date().timeIntervalSince1970 * 1000);
+            print("Device orientation change detected at "+String(currentTime))
+            if (currentTime - self.lastUpdateTime > 1000) {
+                try! self.sceneManager!.loadCurrentScene()
+                print("Reloading current scene")
+            }
+            self.lastUpdateTime = currentTime
+        }
+    }
 }
-
